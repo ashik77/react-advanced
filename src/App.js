2e06dd89b6270655f532e017a564fde12646cbd3
MD ASHIQUR RAHMAN <ashikaman77@gmail.com>
2022-06-02 13:49:06 +0300

    fetching data with hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Counter from "./hooks/Counter";
+import Users from "./hooks/Users";
 
 function App() {
-  return <Counter />;
+  return <Users />;
 }
 
 export default App;
